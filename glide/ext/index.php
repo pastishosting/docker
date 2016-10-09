cff259fa0c3317603e0573af71d8a802dfd2d236
Tristan Rivoallan <tristan@rivoallan.net>
2016-10-09 22:47:08 +0200

    [glide] add manipulators

--- glide/ext/index.php
+++ glide/ext/index.php
@@ -6,6 +6,17 @@ $loader = require '../vendor/autoload.php';
 $manipulators = [
     new League\Glide\Manipulators\Size(2000*2000),
     new League\Glide\Manipulators\Encode(),
+    new League\Glide\Manipulators\Background(),
+    new League\Glide\Manipulators\Blur(),
+    new League\Glide\Manipulators\Border(),
+    new League\Glide\Manipulators\Brightness(),
+    new League\Glide\Manipulators\Contrast(),
+    new League\Glide\Manipulators\Crop(),
+    new League\Glide\Manipulators\Filter(),
+    new League\Glide\Manipulators\Gamma(),
+    new League\Glide\Manipulators\Orientation(),
+    new League\Glide\Manipulators\Pixelate(),
+    new League\Glide\Manipulators\Watermark(),
 ];
 
 // Set image manager
