6a5fb4b467433ba24166b92ee8cbdbcace5618d2
Tristan Rivoallan <tristan@rivoallan.net>
2016-10-11 01:06:56 +0200

    stuff!

--- traefik/hooks/recreate-container.sh
+++ traefik/hooks/recreate-container.sh
@@ -6,7 +6,5 @@ COMPOSE_FILE=$2
 
 if [ $EVENT == "up" ]; then
     # Ugly workaround
-    docker-compose -f $COMPOSE_FILE stop
-    docker-compose -f $COMPOSE_FILE rm -f
-    docker-compose -f $COMPOSE_FILE up -d
+    docker-compose up --build -d
 fi
