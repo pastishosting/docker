fbdc578f4bab8a057e8312985152ac58904fe384
Tristan Rivoallan <tristan@rivoallan.net>
2016-10-12 11:51:28 +0200

    hooks

--- services/plesk/hooks/up.sh
+++ services/plesk/hooks/up.sh
@@ -13,5 +13,5 @@ docker-compose -f $COMPOSE_FILE exec plesk service ssh start
 docker-compose -f $COMPOSE_FILE exec plesk service sw-cp-server restart
 
 # Deploy SSH keys
-docker-compose -f $COMPOSE_FILE exec plesk bash -c 'if [ ! -d /root/.ssh ]; then ssh-keygen -N "" -f /root/.ssh/id_rsa; fi'
-docker-compose -f $COMPOSE_FILE exec plesk echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBtkxRXehGPwJ0KKcyrXq9o2/hfEt06vjcZLakRWHMaJD0WTJrKNNn1Mq+bKf6wJkTW2CWDnjiTMFcqQaTUQfn0bcNhnPgZ6zyYFd/SiC2kZRuvnVYP2kV7MZMvgnEQjrpxCd7mxOmhih1gv68SSk94MmEVXBhjQEVZsFJHyBaNp++NY2+JsjYyuFwPURH+3XcJS3H8QEyVOnnFzJ7ZOo/egk3FoQMmbljgSHMg/jgrIQIAMtFS2PFa0oLUH6+nAZpbS4mNufN7L6T5iwgMAkbrO3Ff/1tQIOu3t/bHKUtwmeUMuKdAz0m2Hu/LImcJBz45u1vsr6ED3qLEEbk9yfx tristan" > /root/.ssh/authorized_keys
+docker-compose -f $COMPOSE_FILE exec plesk bash -xc 'if [ ! -d /root/.ssh ]; then ssh-keygen -N "" -f /root/.ssh/id_rsa; fi'
+docker-compose -f $COMPOSE_FILE exec plesk bash -xc 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBtkxRXehGPwJ0KKcyrXq9o2/hfEt06vjcZLakRWHMaJD0WTJrKNNn1Mq+bKf6wJkTW2CWDnjiTMFcqQaTUQfn0bcNhnPgZ6zyYFd/SiC2kZRuvnVYP2kV7MZMvgnEQjrpxCd7mxOmhih1gv68SSk94MmEVXBhjQEVZsFJHyBaNp++NY2+JsjYyuFwPURH+3XcJS3H8QEyVOnnFzJ7ZOo/egk3FoQMmbljgSHMg/jgrIQIAMtFS2PFa0oLUH6+nAZpbS4mNufN7L6T5iwgMAkbrO3Ff/1tQIOu3t/bHKUtwmeUMuKdAz0m2Hu/LImcJBz45u1vsr6ED3qLEEbk9yfx tristan" > /root/.ssh/authorized_keys'
