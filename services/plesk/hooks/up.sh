d21274254ce103f6d293632635a16a5871c32158
Tristan Rivoallan <tristan@rivoallan.net>
2017-11-06 10:41:56 +0100

    Update up.sh

--- services/plesk/hooks/up.sh
+++ services/plesk/hooks/up.sh
@@ -22,6 +22,7 @@ docker-compose -f $COMPOSE_FILE exec plesk service sw-cp-server restart
 # Deploy SSH keys
 docker-compose -f $COMPOSE_FILE exec plesk bash -xc 'if [ ! -d /root/.ssh ]; then ssh-keygen -N "" -f /root/.ssh/id_rsa; fi'
 docker-compose -f $COMPOSE_FILE exec plesk bash -xc 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBtkxRXehGPwJ0KKcyrXq9o2/hfEt06vjcZLakRWHMaJD0WTJrKNNn1Mq+bKf6wJkTW2CWDnjiTMFcqQaTUQfn0bcNhnPgZ6zyYFd/SiC2kZRuvnVYP2kV7MZMvgnEQjrpxCd7mxOmhih1gv68SSk94MmEVXBhjQEVZsFJHyBaNp++NY2+JsjYyuFwPURH+3XcJS3H8QEyVOnnFzJ7ZOo/egk3FoQMmbljgSHMg/jgrIQIAMtFS2PFa0oLUH6+nAZpbS4mNufN7L6T5iwgMAkbrO3Ff/1tQIOu3t/bHKUtwmeUMuKdAz0m2Hu/LImcJBz45u1vsr6ED3qLEEbk9yfx tristan" > /root/.ssh/authorized_keys'
+docker-compose -f $COMPOSE_FILE exec plesk bash -xc 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+GfisIGwH01yz4iaSR4rs4c4LbqQuyGcKJNFSEo9wI/o3IdXsRpk4zmEXrpSBgWi1JzgNddSFEhZ3gffitrSDEN+XDW2MRxgUyMHgRI+0R5hHt6QGKFJSa5QAcPqG3fCG/NhFLD5Kk/++y/Kl+4XP9jX23jvgugCNsIJmpgk2xgIb4imFHe6vyt35NmS/PY7MMwa2TBLOrCe35N5h6AouWnESoyANXotUXL7ktgPVefzR+e2hX1IDHHp4Ge2+Pxfj+VijG8W7AEzzpMHlzYHR1P5s4CnXW3wGjvzXQRphIzl3hyWJYreP/zEMDOqt2qehibwWHWXL2ZXfUixCMngt johan" >> /root/.ssh/authorized_keys'
 
 # Reload Traefik
 docker-compose -f $SCRIPT_DIR/../../../services/traefik/docker-compose-$(hostname).yml up --build -d
